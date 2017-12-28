a44fa350482040ee991c680e393a2184545f2ab4
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2017-12-28 15:33:01 +0900

    updated

--- indicators.sh
+++ indicators.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash 
 
 function indicators() {
-    sudo -v
-    sudo add-apt-repository ppa:atareao/atareao ppa:fossfreedom/indicator-sysmonitor ppa:indicator-multiload/stable-daily ppa:alexmurray/indicator-sensors-daily ppa:atareao/atareao
+    indlist=( ppa:atareao/atareao \
+		  ppa:fossfreedom/indicator-sysmonitor \
+	          ppa:indicator-multiload/stable-daily \
+		  ppa:dgadomski/indicator-sensors )
+    
+    for i in "${indlist[@]}"
+    do
+	# sudo add-apt-repository -y $i
+	echo $i
+    done
 
-    sudo apt-get update
-    sudo apt-get install indicator-multiload indicator-sysmonitor caffeine calendar-indicator indicator-sensors lm-sensors indicator-cpufreq my-weather-indicator -y
+    # sudo apt-get update
+    sudo apt-get install -y indicator-multiload indicator-sysmonitor caffeine calendar-indicator indicator-sensors  indicator-cpufreq my-weather-indicator
 }
+
+indicators
