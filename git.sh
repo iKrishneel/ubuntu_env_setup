089f59dfa079dcf72b0b4ae4c0734c6abb6d1433
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2021-04-06 11:55:33 +0900

    added git

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get install git
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
+# git configuration
+# force use ssh
+git config --global url."git@github.com:".insteadOf "https://github.com/" 
