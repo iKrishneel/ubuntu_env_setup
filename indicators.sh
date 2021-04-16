ff261dee2fb881a551ad8736dbf9ee54d5896b00
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2021-04-16 14:56:37 +0900

    updated indicators

--- indicators.sh
+++ indicators.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash 
 
-function indicators() {
-    indlist=( ppa:atareao/atareao \
-		  ppa:fossfreedom/indicator-sysmonitor \
-	          ppa:indicator-multiload/stable-daily \
-		  ppa:dgadomski/indicator-sensors )
-    
-    for i in "${indlist[@]}"
-    do
-	# sudo add-apt-repository -y $i
-	echo $i
-    done
-
-    # sudo apt-get update
-    sudo apt-get install -y indicator-multiload indicator-sysmonitor caffeine calendar-indicator indicator-sensors  indicator-cpufreq my-weather-indicator
-}
-
-indicators
+sudo apt-get update
+sudo apt-get install -y \
+     caffeine \
+     indicator-multiload \
+     indicator-cpufreq \
+     indicator-sensors     
