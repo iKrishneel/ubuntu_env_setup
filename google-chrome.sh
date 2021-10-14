87d29e55450e83598c974a5dc7eb16d9829fc4a5
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2021-10-14 13:23:16 +0900

    minor version changes

--- google-chrome.sh
+++ google-chrome.sh
@@ -4,3 +4,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
 sudo apt-get install -f
+sudo rm -rf google-chrome*.deb
--- solarized-theme.sh
+++ solarized-theme.sh
@@ -3,9 +3,12 @@
 sudo apt-get install wget
 
 cd $HOME/Downloads
-wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTAyNTAwMTMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjNlMmZmNmYxMzI4MTc4Y2Q5NzgxN2U2MTgyYTBjNGJiYmVlZTRmMjMyOGFlNDIxYWQ4NmE2NjczMjMyMWQ1MGNhZGY5ZGJmMGVmOWU2MzQ4NDQwYWIzNmVjZGIyNmQwMjg2YmRhNTJlOGEzMmY5NDRkN2Y4N2M1NDcwYjQyYWMwIiwidCI6MTYxODUwMDYyNCwic3RmcCI6ImQ1ZGRkN2ExMDA3NjhlMWVkMGJkZDQxNTQxMzZmM2ViIiwic3RpcCI6IjE1My4yMDIuMzIuMjIwIn0.pz-E9PQF41NAbf3TdbyGWC1Nl5WpDWRuRVJWrev_nkc/Solarized-Dark-Blue-3.36_2.0.4.zip
+rm -rf Solarized-Dark-Blue*
 
-unzip Solarized-Dark-3.36.zip
+wget https://dl3.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTAyNTAwMTMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6Ijg3N2FmYzllOTM4Nzk4OTBiZmNmNGY5YmViZmMzYWQyNDk5M2MxYWRlNjVmMmU2NmRiZmIzZGQ0MjQxZmI4MDdlZjgwYWQwYzRiNWJmY2E2MDYxMjQzNTdkZWVmMzM5OGRlZGNlMmViMjQzMjhiMzI1MTE3NDM4OTEwN2E5YTA1IiwidCI6MTYzNDEyOTY4Miwic3RmcCI6ImNjYTVlNDdmZjk4NjkzZjNhMDcyYTE4YmZhNWNmNjJlIiwic3RpcCI6IjE1My4yMDIuMzIuMjIwIn0.W9RQy4yra4RV1Lrx6YGiGl9K6WSYE5QAXgZ6e_ehjuE/Solarized-Dark-Blue-3.36_2.0.4.zip -O Solarized-Dark-Blue.zip
 
-sudo mv Solarized-Dark-* /usr/share/themes/
-gsettings set org.gnome.desktop.interface gtk-theme "Solarized-Dark-Blue-3.36"
+unzip Solarized-Dark-Blue.zip
+mv Solarized-Dark-Blue-* Solarized-Dark-Blue
+
+sudo mv Solarized-Dark-Blue /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Solarized-Dark-Blue"
